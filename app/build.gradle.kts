e683831f4bb4bcd949f0f2fc3e2053d2885f991f
rsookram <rashadsookram@gmail.com>
2019-03-03 11:27:38 -0500

    Migrate app/build.gradle to Kotlin DSL

---
+++ app/build.gradle.kts
@@ -0,0 +1,69 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-android-extensions")
+}
+
+android {
+    compileSdkVersion(Versions.targetSdk)
+
+    defaultConfig {
+        applicationId = "io.github.rsookram.mediaplayer"
+        minSdkVersion(Versions.minSdk)
+        targetSdkVersion(Versions.targetSdk)
+        versionCode = 1
+        versionName = "1.0"
+
+        resConfigs("en")
+    }
+
+    compileOptions {
+        setTargetCompatibility(JavaVersion.VERSION_1_8)
+    }
+
+    signingConfigs {
+        getByName("debug") {
+            storeFile = file("debug.keystore")
+            storePassword = "android"
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+        }
+    }
+
+    buildTypes {
+        getByName("debug") {
+            setSigningConfig(signingConfigs.getByName("debug"))
+        }
+        getByName("release") {
+            isShrinkResources = true
+            isMinifyEnabled = true
+            proguardFiles("proguard-android.txt", "proguard-rules.pro")
+
+            // Just for testing release builds. Not actually distributed.
+            setSigningConfig(signingConfigs.getByName("debug"))
+        }
+    }
+
+    packagingOptions {
+        exclude("/kotlin/**")
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.allWarningsAsErrors = true
+}
+
+dependencies {
+    implementation(Versions.appCompat)
+    implementation(Versions.constraintLayout)
+
+    implementation(Versions.ktxCore)
+
+    implementation(Versions.exoplayerCore)
+    implementation(Versions.exoplayerUi)
+    implementation(Versions.exoplayerHls)
+
+    implementation(Versions.kotlinStdlib)
+}
