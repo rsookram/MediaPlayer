b070a6c6c7beabeb86bbc0fce94e0da31ebda2fb
rsookram <rashadsookram@gmail.com>
2019-04-29 18:33:54 -0400

    Update dependencies
    - Gradle
    - AGP
    - Kotlin
    - KTX core

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
 
-    private const val kotlin = "1.3.21"
+    private const val kotlin = "1.3.31"
 
     // 2.9.3 works fine, but 2.9.4 is stricter and fails to play some invalid files
     // https://github.com/google/ExoPlayer/commit/32bad6915851b39a48d5385b1268a87d87d7ff20#diff-bf2270d195b25f7412752dbbd0d3fecaR120
@@ -9,13 +9,13 @@ object Versions {
     val minSdk = 23
     val targetSdk = 28
 
-    val androidGradlePlugin = "com.android.tools.build:gradle:3.3.1"
+    val androidGradlePlugin = "com.android.tools.build:gradle:3.4.0"
     val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin"
 
     val appCompat = "androidx.appcompat:appcompat:1.0.2"
     val constraintLayout = "androidx.constraintlayout:constraintlayout:1.1.3"
 
-    val ktxCore = "androidx.core:core-ktx:1.0.0"
+    val ktxCore = "androidx.core:core-ktx:1.0.1"
 
     val exoplayerCore = "com.google.android.exoplayer:exoplayer-core:$exoplayer"
     val exoplayerUi = "com.google.android.exoplayer:exoplayer-ui:$exoplayer"
