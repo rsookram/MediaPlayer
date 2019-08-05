71560e17c02e1aca8f3f6a6916008705fc915448
rsookram <rashadsookram@gmail.com>
2019-08-05 13:05:31 -0400

    Increase minSdkVersion to 25
    from 23.
    
    This decreases APK size by ~30KB.

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -6,7 +6,7 @@ object Versions {
     // https://github.com/google/ExoPlayer/commit/32bad6915851b39a48d5385b1268a87d87d7ff20#diff-bf2270d195b25f7412752dbbd0d3fecaR120
     private const val exoplayer = "2.9.3"
 
-    const val minSdk = 23
+    const val minSdk = 25
     const val targetSdk = 28
 
     const val androidGradlePlugin = "com.android.tools.build:gradle:3.4.2"
