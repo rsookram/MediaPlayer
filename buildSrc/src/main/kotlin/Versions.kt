57133ce323ff259ebbbaeaa5bb79cc998d35ecf9
rsookram <rashadsookram@gmail.com>
2020-10-24 12:09:53 -0400

    Update Kotlin to 1.4.10
    from 1.4.0.

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -6,7 +6,7 @@ object Versions {
     const val targetSdk = 29
 
     const val androidGradlePlugin = "com.android.tools.build:gradle:4.0.1"
-    const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.0"
+    const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10"
 
     const val ktxCore = "androidx.core:core-ktx:1.3.2"
 
