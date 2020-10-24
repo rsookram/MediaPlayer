8a21dc45b0503741c575b4330de7d2ad45a80863
rsookram <rashadsookram@gmail.com>
2020-10-24 12:09:08 -0400

    Update ktx to 1.3.2
    from 1.3.1.

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -8,7 +8,7 @@ object Versions {
     const val androidGradlePlugin = "com.android.tools.build:gradle:4.0.1"
     const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.0"
 
-    const val ktxCore = "androidx.core:core-ktx:1.3.1"
+    const val ktxCore = "androidx.core:core-ktx:1.3.2"
 
     const val exoplayerCore = "com.google.android.exoplayer:exoplayer-core:$exoplayer"
     const val exoplayerUi = "com.google.android.exoplayer:exoplayer-ui:$exoplayer"
