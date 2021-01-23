6843ee7549874c006c075b100827c0b9ee1d7aa9
rsookram <rashadsookram@gmail.com>
2021-01-23 15:07:53 -0500

    Update AGP to 4.1.2
    from 4.1.1.

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,7 +5,7 @@ object Versions {
     const val minSdk = 25
     const val targetSdk = 30
 
-    const val androidGradlePlugin = "com.android.tools.build:gradle:4.1.1"
+    const val androidGradlePlugin = "com.android.tools.build:gradle:4.1.2"
     const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21"
 
     const val ktxCore = "androidx.core:core-ktx:1.3.2"
