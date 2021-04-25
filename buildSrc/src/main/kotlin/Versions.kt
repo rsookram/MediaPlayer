79bf8f38e3e5bb0df0354f22f8df4efe82309596
rsookram <rashadsookram@gmail.com>
2021-04-25 14:55:19 -0400

    Update Kotlin to 1.4.32
    from 1.4.21.

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -6,7 +6,7 @@ object Versions {
     const val targetSdk = 30
 
     const val androidGradlePlugin = "com.android.tools.build:gradle:4.1.2"
-    const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21"
+    const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32"
 
     const val ktxCore = "androidx.core:core-ktx:1.3.2"
 
