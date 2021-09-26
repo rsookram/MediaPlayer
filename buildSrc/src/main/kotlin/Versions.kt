a7baf5d3823e5e118186a9d7a1625c0e45906835
rsookram <rashadsookram@gmail.com>
2021-09-26 14:39:59 -0400

    Update dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,12 +1,12 @@
 object Versions {
 
-    private const val exoplayer = "2.15.0"
+    private const val exoplayer = "2.15.1"
 
     const val minSdk = 25
     const val targetSdk = 30
 
     const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.2"
-    const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30"
+    const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31"
 
     const val ktxCore = "androidx.core:core-ktx:1.6.0"
 
